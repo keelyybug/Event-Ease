90108da35fc52f24fa1127bc206cde7ec4be95e6
Emily McLean <emilymclean93@gmail.com>
2023-05-09 19:25:53 -0400

    fixed bugs in seed.js file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const exphbs = require('express-handlebars');
 const session = require('express-session');
 //const routes = require('./controllers');
 
-const sequelize = require('./config/connection');
+const sequelize = require('./config/config');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
