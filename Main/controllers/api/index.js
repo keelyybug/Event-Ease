4ea8bbfe29731738f45fdf15a8a0e2efcb1a89f1
Ethan Garrison <egarrisxn@gmail.com>
2023-05-09 14:04:41 -0400

    debug

--- Main/controllers/api/index.js
+++
@@ -1,4 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-
-module.exports = router;
--- Main/db/schema.sql
+++
@@ -1,2 +0,0 @@
-DROP DATABASE IF EXISTS event_manager_db;
-CREATE DATABASE event_manager_db;
