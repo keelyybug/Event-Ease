f2ce75cc475050ef82881a6944c8adb18f2906ee
Ethan Garrison <egarrisxn@gmail.com>
2023-05-09 19:26:54 -0400

    debugging

--- models/index.js
+++ models/index.js
@@ -3,22 +3,31 @@ const Rsvp = require('./rsvp');
 const User = require('./user');
 
 User.hasMany(Event, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+  foreignKey: 'user_id',
+  //onDelete: 'CASCADE'
   });
 
+Event.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
 User.hasMany(Rsvp, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-  });
-  
-  Event.belongsTo(User, {
-    foreignKey: 'user_id'
-  });
-  
-  Rsvp.belongsTo(User, {
-    foreignKey: 'user_id'
-  });
-  
+  foreignKey: 'user_id',
+  //onDelete: 'CASCADE'
+});
+
+Rsvp.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Event.hasMany(Rsvp, {
+  foreignKey: 'event_id',
+  //onDelete: 'CASCADE'
+});
+
+Rsvp.belongsTo(Event, {
+  foreignKey: 'event_id'
+});
+
 
 module.exports = { Event, Rsvp, User };
