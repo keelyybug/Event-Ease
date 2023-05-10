f2d4fe9df2736d75889b1fc245243f439b5402a8
Emily McLean <emilymclean93@gmail.com>
2023-05-10 12:04:25 -0400

    user login, logout, signup functionality + routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,21 +1,30 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+//* Create new user (sign up)
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      birthdate: req.body.birthdate
+    });
 
     req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
+      req.session.loggedIn = true;
 
-      res.status(200).json(userData);
+      res.status(200).json(dbUserData);
     });
   } catch (err) {
-    res.status(400).json(err);
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 
+//* log in 
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
@@ -48,6 +57,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
+//* log out
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
