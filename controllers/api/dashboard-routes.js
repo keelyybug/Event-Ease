0975e1e67128715bd242b1f5e10e45fcea846f9f
Emily McLean <emilymclean93@gmail.com>
2023-05-10 13:54:25 -0400

    changes to dashboard routes

--- controllers/api/dashboard-routes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const sequelize = require('../../config/config');
-const { Event, Rsvp, User } = require('../../models');
-const withAuth = require('../../utils/auth');
-
-module.exports = router;
