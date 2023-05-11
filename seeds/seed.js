2e73facfa0a7397b41c8d56e9c0227eca94ea5c3
Ashleyg5 <ashleygiraldo5@hotmail.com>
2023-05-11 11:04:58 -0400

    fixed seed issue

--- seeds/seed.js
+++ seeds/seed.js
@@ -8,15 +8,20 @@ const userData = require('./userData.json');
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  const seedUsers = () => User.bulkCreate(userData);
-  seedUsers();
+  const seedUsers = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true
+  });
+  seedUsers;
 
-  const seedEvents = () => User.bulkCreate(eventData)
-  seedEvents();
+ 
+  const seedEvents = await Event.bulkCreate(eventData);
+  seedEvents;
 
-  const seedRsvp = () => User.bulkCreate(rsvpData)
-  seedUsers();
-
+  const seedRsvp = await Rsvp.bulkCreate(rsvpData);
+  seedRsvp;
 };
+
+
 
 seedAll();
