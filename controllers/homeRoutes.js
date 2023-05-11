f25225116448440392227c3bff5ffb67fb6e860b
Ethan Garrison <egarrisxn@gmail.com>
2023-05-10 23:34:12 -0400

    more functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,30 +6,49 @@ const withAuth = require('../utils/auth');
 router.get('/', (req, res) => {
     res.render('homepage', { title: 'Homepage' });
   });
-  
-  
-  // Login Page
-  router.get('/login', (req, res) => {
-    res.render('login', { title: 'Login' });
-  });
-  
-  
-  // Signup Page
-  router.get('/signup', (req, res) => {
-    res.render('signup', { title: 'Signup' });
-  });
-  
-  
-  // Profile
-  router.get('/profile', (req, res) => {
-    res.render('profile', { title: 'profile' });
-  });
-  
-  
-  // New Event Page
-  router.get('/new-event', (req, res) => {
-    res.render('new-event', { title: 'New Event' });
-  });
+
+
+// Login withAuth middleware && Profile
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true, title: 'Profile'
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login', { title: 'Login' });
+});
+
+
+// Signup Page
+router.get('/signup', (req, res) => {
+  res.render('signup', { title: 'Signup' });
+});
+
+
+// New Event Page
+router.get('/new-event', (req, res) => {
+  res.render('new-event', { title: 'New Event' });
+});
   
   router.post('/create', (req, res) => {
       const newItem = req.body;
@@ -71,27 +90,33 @@ router.get('/', (req, res) => {
   });
   
   
-  // Comments page
-  router.get('/comments', (req, res) => {
-    res.render('comments', { title: 'Comments' });
-  });
+// Comments page
+router.get('/comments', (req, res) => {
+  res.render('comments', { title: 'Comments' });
+});
+
+router.post('/comments', (req, res) => {
+  const { name } = req.body;
+
+  res.sendStatus(200);
+});
   
   
-  // Rsvp Page
-  router.get('/rsvp', (req, res) => {
-    res.render('rsvp', { title: 'Rsvp' });
-  });
-  
-  router.post('/rsvp', (req, res) => {
-    const { name, email } = req.body;
-  
-    res.sendStatus(200);
-  });
-  
-  
-  //Dashboard
-  router.get('/dashboard', (req, res) => {
-    res.render('dashboard', { title: 'Dashboard' });
-  });
+// Rsvp Page
+router.get('/rsvp', (req, res) => {
+  res.render('rsvp', { title: 'Rsvp' });
+});
+
+router.post('/rsvp', (req, res) => {
+  const { name, email } = req.body;
+
+  res.sendStatus(200);
+});
+
+
+//Dashboard
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard', { title: 'Dashboard' });
+});
 
 module.exports = router;
